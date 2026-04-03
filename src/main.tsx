7d9728855ac088f7f48f0c01d2205438bc2de39f
Vu Phan <vu.phan.se@gmail.com>
2026-04-03 21:26:44 +0700

    feat: add radix shell foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App.js";
+import "./app/shell.css";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element not found");
